31b5106d1938cda8fd209b21980f3e2a5df0c477
Kinga <zawarczynska.ko@gmail.com>
2020-02-25 15:56:12 +0100

    added routing to the navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import Home from './screens/Home';
+import HeShe from './screens/HeShe';
+import Services from './screens/Services';
+import WeddingHall from './screens/WeddingHall';
+import Timetable from './screens/Timetable';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Natalia&Konrad</h1>
+        <Nav />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/Couple" component={HeShe} />
+          <Route path="/Services" component={Services} />
+          <Route path="/Place" component={WeddingHall} />
+          <Route path="/Timetable" component={Timetable} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
