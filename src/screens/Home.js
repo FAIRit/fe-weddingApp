233158327c02a69c0b16cb34fe42391c75f53d21
Kinga <zawarczynska.ko@gmail.com>
2020-02-26 13:13:45 +0100

    new font for content in home and some additional styles in home module css

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,12 +6,12 @@ function Home() {
         <div className={styles.home}>
             <h2>Pobieramy się!</h2>
             <div className={styles.church}>
-                <div><i class="fas fa-church"> Ślub</i></div>
+                <div className={styles.bold}><i class="fas fa-church"></i>  Ślub</div>
                 <h3>04.09.2021r. Godzina</h3>
                 <p>Kościół</p>
             </div>
             <div className={styles.weddingHall}>
-                <div><i class="fas fa-glass-cheers"> Wesele</i></div>
+                <div className={styles.bold}><i class="fas fa-glass-cheers"></i>  Wesele</div>
                 <h3>Sala weselna</h3>
                 <p>Godzina</p>
             </div>
