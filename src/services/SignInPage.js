2d2fd2ac9e7469163be29100895490d419ebd87b
Kinga <zawarczynska.ko@gmail.com>
2020-03-04 13:10:14 +0100

    trying to sign in

--- src/services/SignInPage.js
+++ src/services/SignInPage.js
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
-import style from './SignInPage.module.css'
+import style from './SignInPage.module.css';
 
-function SignInPage() {
-    const [alreadySignedIn, setAlreadySignedIn] = useState(true)
+function SignInPage(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null)
 
+    const onSubmit = event => {
+        props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                setEmail(email)
+                setPassword(password)
+                console.log(email, password, 'dobrze zalogowałes')
+                props.history.push("/");
+                debugger
+            })
+            .catch(error => {
+                setError(error);
+            });
+        event.preventDefault();
+    };
 
+    const isInvalid = password === '' || email === '';
     return (
         <div>
             <form>
                 <div className={style.loginLabel}>
                     <label>Login</label>
-                    <input type="name"></input>
+                    <input
+                        name="email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                        type="text"
+                        placeholder="Email Address"
+                    ></input>
                 </div>
                 <div className={style.loginLabel}>
                     <label>Hasło</label>
-                    <input type="password"></input>
+                    <input
+                        name="password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                        type="password"
+                        placeholder="Password"></input>
                 </div>
                 <div className={style.submit}>
-                    <button>Zaloguj</button>
-                </div>
+                    <button disabled={isInvalid} type="submit" onClick={() => {
+                        onSubmit()
+                        }}>
+                        Zaloguj
+                    </button>
 
+                </div>
+                {error && <p>{error.message}</p>}
             </form>
 
         </div>
